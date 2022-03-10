451fa31080d004f161139c4409a0867bb99d0322
willsawyer2609 <wmsawyer2609@gmail.com>
2022-03-10 19:25:02 +1000

    Add community card functions

--- Table.java
+++ Table.java
@@ -1,3 +1,48 @@
-public class Table {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Table {
+    private Deck deck;
+    private List<Player> players;
+    private List<Card> flop;
+    private Card turn;
+    private Card river;
+
+    public Table(Deck deck) {
+        this.deck = deck;
+        players = new ArrayList<Player>();
+        flop = new ArrayList<Card>(3);
+        turn = null;
+        river = null;
+    }
+
+    public void addPlayers(List<Player> players) {
+        this.players.addAll(players);
+    }
+
+    public List<Card> getFlop() {
+        return flop;
+    }
+
+    public Card getTurn() {
+        return turn;
+    }
+
+    public Card getRiver() {
+        return river;
+    }
+
+    public void generateFlop() {
+        flop.add(deck.generateCard());
+        flop.add(deck.generateCard());
+        flop.add(deck.generateCard());
+    }
+
+    public void generateTurn() {
+        turn = deck.generateCard();
+    }
+
+    public void generateRiver() {
+        river = deck.generateCard();
+    }
 }
